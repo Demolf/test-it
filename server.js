a0f325c4cc38f3e37f9e8bca10cf8dd9aaf4039c
Demolf <43671792+Demolf@users.noreply.github.com>
2026-02-17 23:31:46 +0400

    Update server.js

--- server.js
+++ server.js
@@ -22,5 +22,6 @@ io.on('connection', (socket) => {
 
 const PORT = 3000;
 server.listen(PORT, () => {
-    console.log(`Сервер запущен на http://localhost:${PORT}`);
+    console.log(`Сервер запущен на сайт`);
 });
+
