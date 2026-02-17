a7b221a35702aed0ddbffa81cbb83ef0fb7589ff
Demolf <43671792+Demolf@users.noreply.github.com>
2026-02-17 23:09:12 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,39 +1,26 @@
-// server.js
 const express = require('express');
 const http = require('http');
-// const WebSocket = require('ws');
+const socketIo = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-// const wss = new WebSocket.Server({ server });
+const io = socketIo(server);
 
-// Раздаём статику
 app.use(express.static('public'));
 
-// Храним всех клиентов
-const clients = new Set();
+io.on('connection', (socket) => {
+    console.log('Пользователь подключился');
 
-// wss.on('connection', (ws) => {
-//     console.log('Новый пользователь подключился');
-//     clients.add(ws);
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
 
-//     ws.on('message', (data) => {
-//         console.log('Сообщение:', data.toString());
-//         // Рассылаем всем, кроме отправителя
-//         clients.forEach((client) => {
-//             if (client !== ws && client.readyState === WebSocket.OPEN) {
-//                 client.send(data.toString());
-//             }
-//         });
-//     });
-
-//     ws.on('close', () => {
-//         console.log('Пользователь отключился');
-//         clients.delete(ws);
-//     });
-// });
+    socket.on('disconnect', () => {
+        console.log('Пользователь отключился');
+    });
+});
 
 const PORT = 3000;
 server.listen(PORT, () => {
-    console.log(`Чат запущен на http://localhost:${PORT}`);
+    console.log(`Сервер запущен на http://localhost:${PORT}`);
 });
