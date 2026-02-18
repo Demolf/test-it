615eea54b94213327a8b8bfbea08027815e91792
Demolf <43671792+Demolf@users.noreply.github.com>
2026-02-18 22:33:02 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,18 @@
 const express = require("express");
 const http = require("http");
-const socketIo = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = require('./server')
 
-const db = require('./db')
+const db = require("./db");
 
 const PORT = 3000;
 
 app.use(express.static("public"));
 
-io.on("connection", (socket) => {
-  console.log("Пользователь подключился");
-
-  socket.on("chat message", (d) => {
-    const sql = "INSERT INTO messages (content, name) VALUES (?, ?)";
-    db.query(sql, [d.msg, d.name], (err, result) => {
-      if (err) {
-        console.error("Ошибка при добавлении сообщения:", err);
-        return;
-      }
-
-      // Отправляем всем (включаем ID и время)
-      io.emit("chat message", {
-        id: result.insertId,
-        msg: d.msg,
-        name: d.name,
-      });
-    });
-  });
-
-  const sql =
-    "SELECT id, content, name, created_at FROM messages ORDER BY created_at ASC";
-  db.query(sql, (err, results) => {
-    if (err) {
-      console.error("Ошибка при добавлении сообщения:", err);
-      return;
-    }
-
-    // Отправляем всем (включаем ID и время)
-    socket.emit("get history", results);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("Пользователь отключился");
-  });
-});
+io.connect(server)
 
 server.listen(PORT, () => {
   console.log(`Сервер запущен на сайт ${PORT}`);
 });
-
-
