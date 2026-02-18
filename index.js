db63103b94db53b29dbfcf538da0685b0ce3b3ec
Demolf <43671792+Demolf@users.noreply.github.com>
2026-02-18 19:36:38 +0400

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ io.on('connection', (socket) => {
 
 const PORT = 3000;
 server.listen(PORT, () => {
-    console.log(`Сервер запущен на сайт`);
+    console.log(`Сервер запущен на сайт ${PORT}`);
 });
+
 
