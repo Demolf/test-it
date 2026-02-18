837eeef63457e9ecd4c2aa2e49ae010d7e3a42e3
LeonId <43671792+Demolf@users.noreply.github.com>
2026-02-18 21:02:32 +0400

    1
    1

--- index.js
+++ index.js
@@ -1,28 +1,70 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const mysql = require("mysql2"); // ← изменили здесь
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(express.static('public'));
-
-io.on('connection', (socket) => {
-    console.log('Пользователь подключился');
-
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-    });
-
-    socket.on('disconnect', () => {
-        console.log('Пользователь отключился');
-    });
-});
-
 const PORT = 3000;
-server.listen(PORT, () => {
-    console.log(`Сервер запущен на сайт ${PORT}`);
+
+// Подключение к MySQL
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root", // измените, если у вас есть пароль
+  database: "chat_db",
 });
 
+// Проверка подключения к БД
+db.connect((err) => {
+  if (err) {
+    console.error("Ошибка подключения к MySQL:", err);
+    return;
+  }
+  console.log("Подключено к MySQL (база: chat_db)");
+});
 
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("Пользователь подключился");
+
+  socket.on("chat message", (d) => {
+    const sql = "INSERT INTO messages (content, name) VALUES (?, ?)";
+    db.query(sql, [d.msg, d.name], (err, result) => {
+      if (err) {
+        console.error("Ошибка при добавлении сообщения:", err);
+        return;
+      }
+
+      // Отправляем всем (включаем ID и время)
+      io.emit("chat message", {
+        id: result.insertId,
+        msg: d.msg,
+        name: d.name,
+      });
+    });
+  });
+
+  const sql =
+    "SELECT id, content, name, created_at FROM messages ORDER BY created_at ASC";
+  db.query(sql, (err, results) => {
+    if (err) {
+      console.error("Ошибка при добавлении сообщения:", err);
+      return;
+    }
+
+    // Отправляем всем (включаем ID и время)
+    socket.emit("get history", results);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Пользователь отключился");
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Сервер запущен на сайт ${PORT}`);
+});
